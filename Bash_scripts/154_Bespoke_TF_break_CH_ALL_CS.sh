f46148d17633f1c046781e23faa295351f35e5d7
My Name <manueltar@hotmail.com>
2025-04-09 12:40:56 +0200

    changed CS

--- Bash_scripts/154_Bespoke_TF_break_CH_ALL_CS.sh
+++ Bash_scripts/154_Bespoke_TF_break_CH_ALL_CS.sh
@@ -30,12 +30,12 @@ seff_name=$(echo "seff""_""$type")
 Rscript_bespoke_consultation=$(echo "$Rscripts_path""449_Inquire_for_TF_changes.R")
 
 
-Final_table_TF_motif_prediction=$(echo "/group/soranzo/manuel.tardaguila/CH/ALL_variants_in_CS/TF_motif_analysis/Final_table_TF_motif_prediction.tsv")
+Final_table_TF_motif_prediction=$(echo "/group/soranzo/manuel.tardaguila/CH/ALL_variants_in_CS/TF_motif_analysis/Final_table_TF_motif_prediction_CS.tsv")
 CHIP_variants_file=$(echo "/group/soranzo/manuel.tardaguila/CH/ALL_variants_in_CS/CHIP_variants.tsv")
 
 # $dependency_string_array
 
-myjobid_bespoke_consultation=$(sbatch --job-name=$name_bespoke_consultation --output=$outfile_bespoke_consultation --partition=cpuq --time=24:00:00 --nodes=1 --ntasks-per-node=2 --mem-per-cpu=4096M --parsable --wrap="Rscript $Rscript_bespoke_consultation --Final_table_TF_motif_prediction $Final_table_TF_motif_prediction --TF_REF $TF_REF --TF_ALT $TF_ALT --CHIP_variants_file $CHIP_variants_file --type $type --out $output_dir")
+myjobid_bespoke_consultation=$(sbatch --job-name=$name_bespoke_consultation --output=$outfile_bespoke_consultation --partition=cpuq --time=24:00:00 --nodes=1 --ntasks-per-node=1 --mem-per-cpu=4096M --parsable --wrap="Rscript $Rscript_bespoke_consultation --Final_table_TF_motif_prediction $Final_table_TF_motif_prediction --TF_REF $TF_REF --TF_ALT $TF_ALT --CHIP_variants_file $CHIP_variants_file --type $type --out $output_dir")
 myjobid_seff_bespoke_consultation=$(sbatch --dependency=afterany:$myjobid_bespoke_consultation --open-mode=append --output=$outfile_bespoke_consultation --job-name=$seff_name --partition=cpuq --time=24:00:00 --nodes=1 --ntasks-per-node=1 --mem-per-cpu=128M --parsable --wrap="seff $myjobid_bespoke_consultation >> $outfile_bespoke_consultation")	
 
 
